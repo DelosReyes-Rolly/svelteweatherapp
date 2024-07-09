d54971587fc1f2dc707267dff3c77e09e8ae8971
rollydelosreyes02 <rolly.a.delosreyes@gmail.com>
2024-07-09 15:03:23 +0800

    UI Changes

--- src/routes/components/Card.svelte
+++ src/routes/components/Card.svelte
@@ -16,55 +16,58 @@
 		<Content class="mdc-typography--body2">
 			<Search {onSearch} /> <br />
 			<div style="height: 400px;" class="card-above">
-				<LayoutGrid>
-					<img
-						src="card-background.jpg"
-						alt="Weather background"
-						style="margin: -2.2em; position: absolute; z-index: 1; height:402px; width:1170px; filter: brightness(60%);"
-						class="card-above"
-					/>
-					<Cell
-						span={4}
-						style="margin: 4em 2em; text-align: center; position: relative; z-index: 2; color: white;"
-					>
-						<div class="left-to-right">
-							<img src={weatherIconUrl} alt="Weather icon" />
-							<h2 class="mdc-typography--headline6" style="margin: 0; font-size: 100px;">
-								{+data.main.temp.toFixed(0)}°C
-							</h2>
+				<img
+					src="card-background.jpg"
+					alt="Weather background"
+					style="position: absolute; z-index: 1; height:402px; width:1170px; filter: brightness(40%);"
+					class="card-above"
+				/>
+				<div
+					id="top-content"
+					style="text-align: center; position: relative; z-index: 2; color: white; padding-top: 80px;"
+				>
+					<div class="left-to-right">
+						<img src={weatherIconUrl} alt="Weather icon" />
+						<h2 class="mdc-typography--headline6" style="margin: 0; font-size: 100px;">
+							{+data.main.temp.toFixed(0)}°C
+						</h2>
+						<br /><br />
+						<h3
+							class="mdc-typography--subtitle6"
+							style="margin: 0 0 10px; letter-spacing: 4px; text-transform: uppercase;"
+						>
 							<br /><br />
-							<h3
-								class="mdc-typography--subtitle6"
-								style="margin: 0 0 10px; letter-spacing: 4px; text-transform: uppercase;"
+							{data.weather[0].description}
+						</h3>
+						<br />
+						<div style="letter-spacing: 4px; text-transform: uppercase; padding-right: 20px;">
+							Humidity - <span style="padding-top: 12px; padding-right: 40px;"
+								>{data.main.humidity}%</span
 							>
-								{data.weather[0].description}
-							</h3>
-							<LayoutGrid>
-								<Cell span={4} style="letter-spacing: 4px; text-transform: uppercase;">
-									Humidity
-									<div style="padding-top: 12px">{data.main.humidity}%</div>
-								</Cell>
-								<Cell span={4}></Cell>
-								<Cell span={4} style="letter-spacing: 4px; text-transform: uppercase;">
-									Wind
-									<div style="padding-top: 12px">{+data.wind.speed.toFixed(0)} k/m</div>
-								</Cell>
-							</LayoutGrid>
+						</div><br />
+						<div style="letter-spacing: 4px; text-transform: uppercase;">
+							Wind - <span style="padding-top: 12px;">{+data.wind.speed.toFixed(0)} k/m</span>
 						</div>
-					</Cell>
-					<Cell span={4}></Cell>
-					<Cell span={4} style="margin: 4em; text-align: center; position: relative; z-index: 2;">
-						<div class="right-to-left">
-							<h2
-								class="mdc-typography--headline6"
-								style="margin: 0; letter-spacing: 4px; text-transform: uppercase; font-size: 28px; color: white;"
-							>
-								{data.name}
-							</h2>
-							<hr style="border-radius: 10px; border: 4px solid white; width: 80px" />
-						</div>
-					</Cell>
-				</LayoutGrid>
+					</div>
+				</div>
+				<div
+					id="top-content"
+					style="text-align: center; position: relative; z-index: 2; color: white;"
+				></div>
+				<div
+					id="top-content"
+					style="text-align: center; position: relative; z-index: 2; color: white; padding-top: 100px;"
+				>
+					<div class="right-to-left">
+						<h2
+							class="mdc-typography--headline6"
+							style="margin: 0; letter-spacing: 4px; text-transform: uppercase; font-size: 28px; color: white;"
+						>
+							{data.name}
+						</h2>
+						<hr style="border-radius: 10px; border: 4px solid white; width: 80px" />
+					</div>
+				</div>
 			</div>
 			{#each fc.slice(1, 6) as fore}
 				<section style="color: white; margin-top: 2em; margin-bottom: 2em;" class="top-to-bottom">
@@ -96,12 +99,12 @@
 		box-shadow:
 			0 4px 8px 0 rgba(0, 0, 0, 0.2),
 			0 6px 20px 0 rgba(0, 0, 0, 0.19);
-		text-shadow: 2px 2px 2px rgb(141, 140, 140);
+		text-shadow: 0.6px 0.6px 0.6px rgb(141, 140, 140);
 	}
 	.card-below {
 		border-radius: 16px;
 		box-shadow:
-			0 4px 8px 0 rgba(0, 0, 0, 0.2),
+			0 4px 8px 0 rgba(22, 19, 19, 0.2),
 			0 6px 20px 0 rgba(0, 0, 0, 0.19);
 		text-shadow: 2px 2px 2px rgb(141, 140, 140);
 		margin-right: 14px;
@@ -116,6 +119,12 @@
 	}
 	section {
 		width: 20%;
+		height: 100%;
+		float: left;
+		box-sizing: border-box; /* so the borders don't mess up the alignment */
+	}
+	#top-content {
+		width: 32%;
 		height: 100%;
 		float: left;
 		box-sizing: border-box; /* so the borders don't mess up the alignment */
